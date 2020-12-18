ad795edebfac97c84401012588c1d0bdb34f28d7
Joan Claverol <joanclaverol@gmail.com>
2020-12-18 22:48:30 +0100

    working on project 1, homemade

--- project-1/app.js
+++ project-1/app.js
@@ -1,49 +1,7 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
-const form = document.querySelector('.quiz-form');
-const result = document.querySelector('.result');
-console.log(result);
+const answers = ['A', 'B', 'B'];
+const questions = document.querySelectorAll('.question');
 
-form.addEventListener('submit', e => {
-    // we don't want to refresh the page, witch is the default action
-    e.preventDefault();
-
-    // declare variable to store the scores
-    let score = 0;
-    // store in an array the results of the form
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-
-    // check the answers
-    userAnswers.forEach((answer, index) => {
-        if (answer === correctAnswers[index]) {
-            score += 25;
-        }
-    });
-
-    // log the score to console
-    // scroll at the begining of the page
-    window.scrollTo(0, 0);
-    // show results on page
-    result.querySelector('span').textContent = `${score}%`;
-    // console.log(result.querySelector('span').textContent);
-    // remove the class of not showing the results 
-    result.classList.remove('d-none');
-    // helps to create an animation
-    let output = 0;
-    const timer = setInterval(() => {
-        result.querySelector('span').textContent = `${output}%`;
-        if (output === score) {
-            clearInterval(timer);
-        } else {
-            output++;
-        }
-    }, 10);
+// get the text for each div
+questions.forEach(question => {
+    console.log(question.innerText)
 });
-
-let i = 0;
-setInterval(() => {
-    console.log('hello');
-    i++;
-    if (i === 5) {
-        clearInterval(timer);
-    }
-}, 1000);
