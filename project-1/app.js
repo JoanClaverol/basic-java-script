c543c97697daaee6c2f727a3599f47bfd37ad95c
Joan Claverol <joanclaverol@gmail.com>
2020-12-20 21:28:55 +0100

    working on project 1 with template form udmy course

--- project-1/app.js
+++ project-1/app.js
@@ -1,7 +1,23 @@
-const answers = ['A', 'B', 'B'];
-const questions = document.querySelectorAll('.question');
+const answers = ['B', 'A', 'B'];
 
-// get the text for each div
-questions.forEach(question => {
-    console.log(question.innerText)
+// moving inside the quizz exercise
+const results = document.getElementsByName('q1');
+
+const results2 = document.getElementsByName('q2');
+results.forEach(result => {
+    if (result.checked) {
+        console.log('Option selected is', result.value);
+    }
 });
+
+function myFunction() {
+    document.getElementById("ninjaForm").submit();
+    //console.log(results2);
+}
+
+// up a level
+// const questions = document.getElementsByClassName('.my-5');
+
+// questions.forEach(question => {
+//     console.log(question);
+// });
