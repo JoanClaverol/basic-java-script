1400261e8391c743615d341abb35dca6c70273b5
Joan Claverol <jclaverol@fluidrahq.fluidra.com>
2020-12-24 13:55:09 +0100

    added remove functionality for event listeners

--- project-2/app.js
+++ project-2/app.js
@@ -1,11 +1,10 @@
 // 1. Add new values on a list
 const input = document.querySelector('.add'); 
-const toDos = document.querySelector('.todos');
-const trashIcons = document.querySelectorAll('.far'); 
+let ul = document.querySelector('ul');
 
 // b. How to insert html on a list?
 function addToDo(toDo){
-    toDos.innerHTML += `
+    ul.innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${toDo}</span>
         <i class="far fa-trash-alt delete"></i>
@@ -17,19 +16,21 @@ input.addEventListener('keydown', e => {
     if(e.key == 'Enter'){
         e.preventDefault(); 
 
-        toDo = input.add.value;  
+        let toDo = input.add.value;  
         input.add.value = ''; 
         
         addToDo(toDo);  
+        ul = document.querySelector('ul'); 
     }
 }); 
 // extra things. Be sure it is unique value, doesn't exist on the list already
-// clean the spaces
-
-function removeToDo() {
-
-}; 
-
 
 // 2. Remove values in the list
+// get all the to dos
+ul.addEventListener('click', e => {
+    if(e.target.className === 'far fa-trash-alt delete'){
+        e.target.parentNode.remove()
+    }
+});
+
 // 3. Search for values in the list
