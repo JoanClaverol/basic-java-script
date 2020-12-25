22863ab9a47feccd2a02db969c67b230b9212db3
Joan Claverol <joanclaverol@gmail.com>
2020-12-25 21:46:34 +0100

    added changes

--- project-2/app.js
+++ project-2/app.js
@@ -1,36 +1,33 @@
 // 1. Add new values on a list
-const input = document.querySelector('.add'); 
-let ul = document.querySelector('ul');
+const input = document.querySelector('.add');
+const toDos = document.querySelector('.todos');
+const trashIcons = document.querySelectorAll('.far');
 
 // b. How to insert html on a list?
-function addToDo(toDo){
-    ul.innerHTML += `
+function addToDo(toDo) {
+    toDos.innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-center">
         <span>${toDo}</span>
         <i class="far fa-trash-alt delete"></i>
     </li>
-    `;  
+    `;
 };
 // a. get the value inside the input form
 input.addEventListener('keydown', e => {
-    if(e.key == 'Enter'){
-        e.preventDefault(); 
+    if (e.key == 'Enter') {
+        e.preventDefault();
 
-        let toDo = input.add.value;  
-        input.add.value = ''; 
-        
-        addToDo(toDo);  
-        ul = document.querySelector('ul'); 
-    }
-}); 
-// extra things. Be sure it is unique value, doesn't exist on the list already
+        toDo = input.add.value;
+        input.add.value = '';
 
-// 2. Remove values in the list
-// get all the to dos
-ul.addEventListener('click', e => {
-    if(e.target.className === 'far fa-trash-alt delete'){
-        e.target.parentNode.remove()
+        addToDo(toDo);
     }
 });
+// extra things. Be sure it is unique value, doesn't exist on the list already
+// clean the spaces
+trashIcons.addEventListener('click', e => {
 
+});
+
+// 2. Remove values in the list
 // 3. Search for values in the list
